e0850de00fd79a62792581a0929d1426ea8575ed
Sergio <techdoer@gmail.com>
2017-01-04 06:27:28 +0100

    A simple dimension table to capture information regarding family members, doctors, payees etc.

---
+++ mssql/People.sql
@@ -0,0 +1,30 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: A dimension table to capture information regarding family members, payees, family doctors etc..  Designed for Azure SQL Server. 
+-- URL:         -
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[People]
+(
+    [id] INT NOT NULL, 
+    [first_name] NVARCHAR(20) NOT NULL, 
+    [last_name] NVARCHAR(20) NOT NULL, 
+	[date_of_birth] INT NULL,
+    [remarks] NVARCHAR(2056) NULL,
+	CONSTRAINT [PK People] PRIMARY KEY ([id]),
+	CONSTRAINT [FK_People-Calendar_date] FOREIGN KEY ([date_of_birth]) REFERENCES [dbo].[Calendar dates] ([date_id])
+)
