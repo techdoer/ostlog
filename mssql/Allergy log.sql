80c28e0be43a8e24ea76905003a0a24b7203a123
Sergio <techdoer@gmail.com>
2017-01-30 21:07:51 +0100

    Tracking allergic disorders by referencing SNOWMED CT identifiers.

---
+++ mssql/Allergy log.sql
@@ -0,0 +1,31 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: A simple fact table to classify common allergies by referencing allergic disorders in the SNOWMED CT Classification. 
+-- URL: 
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Allergy log]
+(
+	[id] INT NOT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[person_id] INT NOT NULL,
+	[allergy_disorder_sctid] INT NOT NULL, -- SNOWMED CT ID for Allergic disorder
+	[remarks] NVARCHAR(2056) NULL,   
+	CONSTRAINT [PK_Allergy log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Allergy log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Allergy log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+)
