d169e4f7ac9d66dd3a52676673fca787c970e166
Sergio <techdoer@gmail.com>
2017-02-16 22:18:46 +0100

    allow for grade desc when grade point not provided

--- mssql/School grades log.sql
+++ mssql/School grades log.sql
@@ -25,14 +25,16 @@ CREATE TABLE [dbo].[School grades log]
 	[person_id] INT NOT NULL,
 	[school_level_isced] INT NOT NULL, -- referencing International Standard Classification of Education ISCED 2011
 	[subject_name] NVARCHAR (512) NOT NULL,
-	[grade_point] INT NOT NULL,
-	[grade_point_minimum] INT NOT NULL, -- maximum value supported by the grading system (e.g. 0)
-	[grade_point_maximum] INT NOT NULL, -- the minimum value supported by the grading system (e.g 10)
+	[grade_point] INT NULL,
+	[grade_desc] NVARCHAR (64) NULL, -- Excellent, Very Good, Good, Accepted, Failed
+	[grade_point_minimum] INT NULL, -- maximum value supported by the grading system (e.g. 0)
+	[grade_point_maximum] INT NULL, -- the minimum value supported by the grading system (e.g 10)
 	[grade_letter] NVARCHAR(1) NULL, -- e.g. A=Excelent, B=Good, C=Average, D=Below Average, F=Failing
 	[remarks] NVARCHAR(2056) NULL,   
 	CONSTRAINT [PK_School grades log] PRIMARY KEY CLUSTERED ([id] ASC),
 	CONSTRAINT [FK_School grades log-Calendar_date1] FOREIGN KEY ([start_calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_School grades log-Calendar_date2] FOREIGN KEY ([end_calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_School grades log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
-	CHECK ([school_level_isced] >= 0 AND [school_level_isced] <= 8)
+	CHECK ([school_level_isced] >= 0 AND [school_level_isced] <= 8),
+	CHECK ([grade_desc] = 'excellent' OR [grade_desc] = 'very good' OR [grade_desc] = 'good' OR [grade_desc] = 'accepted' OR [grade_desc] = 'failed')
 )
