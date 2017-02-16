7dddda35f3166f0b6f0ce8a1ec0107af844b51ed
Sergio <techdoer@gmail.com>
2017-02-16 06:50:23 +0100

    Track school gradses from primary school until completion of university.

---
+++ mssql/School grades log.sql
@@ -0,0 +1,38 @@
+﻿-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: A simple fact table to record school grades from primary school through to university. 
+-- URL: 
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[School grades log]
+(
+	[id] INT NOT NULL,
+	[start_calendar_date_id] INT NOT NULL,
+	[end_calendar_date_id] INT NOT NULL,
+	[person_id] INT NOT NULL,
+	[school_level] INT NOT NULL, -- referencing International Standard Classification of Education ISCED 2011
+	[subject_name] NVARCHAR (512) NOT NULL,
+	[grade_point] INT NOT NULL,
+	[grade_point_minimum] INT NOT NULL, -- maximum value supported by the grading system (e.g. 0)
+	[grade_point_maximum] INT NOT NULL, -- the minimum value supported by the grading system (e.g 10)
+	[grade_letter] NVARCHAR(1) NULL, -- e.g. A=Excelent, B=Good, C=Average, D=Below Average, F=Failing
+	[remarks] NVARCHAR(2056) NULL,   
+	CONSTRAINT [PK_School grades log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_School grades log-Calendar_date1] FOREIGN KEY ([start_calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_School grades log-Calendar_date2] FOREIGN KEY ([end_calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_School grades log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+	CHECK ([school_level] >= 0 AND [school_level] <= 8)
+)
