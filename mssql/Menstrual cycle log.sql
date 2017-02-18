9f440f8298d1ea3b69fb60694fb9f5fe82d5442f
Sergio <techdoer@gmail.com>
2017-02-18 16:59:29 +0100

    updated flow values

--- mssql/Menstrual cycle log.sql
+++ mssql/Menstrual cycle log.sql
@@ -25,11 +25,11 @@ CREATE TABLE [dbo].[Menstrual cycle log]
 	[person_id] INT NOT NULL,
 	[timestamp] TIME(0) NOT NULL,
 	[day_in_cycle] INT NOT NULL, -- 1 = first day, 2 = second day....
-	[flow] INT NULL, -- 1 = mild ... 3 = intense
+	[flow] NVARCHAR(20) NULL,
 	[remarks] NVARCHAR(2056) NULL, 
 	CONSTRAINT [PK_Menstrual cycle log] PRIMARY KEY CLUSTERED ([id] ASC),
 	CONSTRAINT [FK_Menstrual cycle log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_Menstrual cycle log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
 	CHECK ([day_in_cycle] >= 1 AND [day_in_cycle] <= 10),
-	CHECK ([flow] >= 1 AND [flow] <= 3)
+	CHECK ([flow] = 'heavy' OR [flow] = 'light' OR [flow] = 'medium' OR [flow] = 'spotting')
 )
