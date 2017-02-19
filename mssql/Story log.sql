ba05545d2c1de24a0c1ded01bcf5fe10e12980b2
Sergio <techdoer@gmail.com>
2017-02-19 17:08:32 +0100

    allows for capturing person specific narratives

---
+++ mssql/Story log.sql
@@ -0,0 +1,32 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: Allows recording person-specific growth/health/happiness related narratives 
+-- URL: 
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Story log]
+(
+	[id] INT NOT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[timestamp] TIME(0) NOT NULL,
+	[person_id] INT NOT NULL, 
+	[text] NVARCHAR(4000) NULL, 
+	[remarks] NVARCHAR (4000) NULL,
+	CONSTRAINT [PK_Story log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Story log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Story log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+)
