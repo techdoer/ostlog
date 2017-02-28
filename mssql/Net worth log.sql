4e40a52d8f6613a649118b3c8ce51d2a97618613
Sergio <techdoer@gmail.com>
2017-02-28 06:08:44 +0100

    simple tracking for assets and liabiities

---
+++ mssql/Net worth log.sql
@@ -0,0 +1,38 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: A simple fact table to track net worth over time.  Designed for Azure SQL Server. 
+-- URL: 
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Net worth log]
+(
+	[id] INT NOT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[domain] NVARCHAR (15) NOT NULL,
+	[person_id] INT NOT NULL, 
+	[cash_usd] INT NULL,
+	[financial_investments_usd] INT NULL,
+	[real_estate_investments_usd] INT NULL,
+	[other_assets_usd] INT NULL,
+	[loans_usd] INT NULL,
+	[credit_card_usd] INT NULL,
+	[other_liabilities_usd] INT NULL,
+	[remarks] NVARCHAR(4000) NULL, 
+	CONSTRAINT [PK_Net worth log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Net worth log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Net worth log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+)
