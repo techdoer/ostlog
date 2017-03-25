e7e9acdef3fa3c556724318d63d76712529637ae
Sergio <techdoer@gmail.com>
2017-03-25 06:51:51 +0100

    Drug targets provides reference infrormation regarding drugs, and the disorders they target.

---
+++ mssql/Drug targets.sql
@@ -0,0 +1,28 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: Each entry describes a drug and the disorder(s) it targets.  Supports Azure SQL Server. 
+-- URL: http://ostlog.org/schema/Drugs.json
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Drug targets] (
+	[drug_atc_code] NVARCHAR(50) NOT NULL,
+	[disorder_sctid_code] INT NOT NULL,
+	[disorder_name] NVARCHAR (50),
+	[remarks] NVARCHAR (4000) NULL,
+	CONSTRAINT [PK_Drug disorder lookup] PRIMARY KEY CLUSTERED ([drug_atc_code] ASC, [disorder_sctid_code] ASC),
+
+)
