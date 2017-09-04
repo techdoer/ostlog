d15dff2ce5076dadd3ff86dd048387b92cc62430
Sergio <techdoer@gmail.com>
2017-09-04 21:49:40 +0200

    include number of days where symptoms observed.  Delete access database which is out of date.

--- mssql/views/View days betweeen menstrual cycle.sql
+++ mssql/views/View days betweeen menstrual cycle.sql
@@ -12,25 +12,18 @@
 -- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
 -- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
--- Version: Each entry charaterizes a diagnosis given by a medical professional. Supports for Azure SQL Server. 
--- URL: http://ostlog.org/schema/Diagnosis_log.json
+-- Version: View [Menstrual cycle log] as a two column table consisting of StartDate | DaysBetweeen where DaysBetween is the days between successive menstural 
+-- cycle start dates
+--  
+-- URL: http://ostlog.org/schema/View_days_between_menstrual_cycle.json
 --
 -- Authors:     Sergio Bogazzi
 -- Copyright:   Copyright (c) 2017 family•smarts
 -- License:     MIT License
-CREATE TABLE [dbo].[Diagnosis log]
-(
-	[id] INT NOT NULL,
-	[calendar_date_id]	INT	NOT NULL,
-	[domain] NVARCHAR (15) NOT NULL,
-	[doctor_id] INT NULL,
-	[patient_id] INT NOT NULL,
-	[diagnosis_icd10_code] NVARCHAR(7) NOT NULL, /* from the WHO's International Statistical Classification of Diseases and Related Health Problems. */
-	[num_days_with_symptoms] INT NULL,
-	[remarks]	NVARCHAR (2056) NULL,
-	CONSTRAINT [PK_Diagnosis log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Diagnosis log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Diagnosis log-People] FOREIGN KEY ([patient_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Diagnosis log-People2] FOREIGN KEY ([doctor_id]) REFERENCES [dbo].[People] ([id]),
-	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
-)
+CREATE VIEW [dbo].[View days between menstrual cycle]
+	AS 
+	
+	SELECT	m.calendar_date_id AS StartDate, m.person_id, DATEDIFF(day, convert(date, convert(nvarchar(8), m.calendar_date_id), 112), 
+			LEAD(convert(date, convert(nvarchar(8), calendar_date_id), 112), 1, GETDATE()) OVER (ORDER BY calendar_date_id ASC)) AS DaysBetween
+	FROM [dbo].[Menstrual cycle log] m
+	WHERE day_in_cycle = 1;
