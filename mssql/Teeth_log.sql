521e603c1982dd6dcc3288a2dfe72461c6facb9b
Sergio <techdoer@gmail.com>
2017-10-29 14:16:50 +0100

    referencing iso 3950 - FDI World Dental Federation notation

--- mssql/Teeth_log.sql
+++ mssql/Teeth_log.sql
@@ -12,7 +12,7 @@
 -- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
 -- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
--- Version: Each entry records an event on a person's tooth identified by ISO3950.  Supports Azure SQL Server.
+-- Version: Each entry records an event on a person's tooth identified by FDI World Dental Federation notation (iso3950).  Supports Azure SQL Server.
 -- URL: http://ostlog.org/schema/Teeth_log.json
 --
 -- Authors:     Sergio Bogazzi
