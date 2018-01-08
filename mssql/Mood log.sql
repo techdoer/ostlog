a9bed6ffcbf087251e5ebbe5fe1d06a92258b152
Sergio <techdoer@gmail.com>
2018-01-08 20:58:03 +0100

    Single word to capture emotions.  Based on Plutchik's theory.

--- mssql/Mood log.sql
+++ mssql/Mood log.sql
@@ -26,12 +26,14 @@ CREATE TABLE [dbo].[Mood log]
 	[domain] NVARCHAR (15) NOT NULL,
 	[person_id] INT NOT NULL,
 	[feeling_great] NVARCHAR(20) NOT NULL,
-	[how_are_you_doing_text] NVARCHAR(4000) NOT NULL,
-	[how_are_you_doing_score] FLOAT NULL,
+	[how_are_you_word] NVARCHAR (100) NULL,
+	[how_are_you_text] NVARCHAR(4000) NOT NULL,
+	[how_are_you_score] FLOAT NULL,
 	[remarks] NVARCHAR(4000) NULL,   
 	CONSTRAINT [PK_Mood log] PRIMARY KEY CLUSTERED ([id] ASC),
 	CONSTRAINT [FK_Mood log-Calendar_date1] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_Mood log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
 	CHECK ([feeling_great] = 'strongly disagree' OR [feeling_great] = 'disagree' OR [feeling_great] = 'neutral' OR [feeling_great] = 'agree' OR [feeling_great] = 'strongly agree'),
-	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
+	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental'),
+	CHECK([how_are_you_word] = 'fear' OR [how_are_you_word] = 'anger' OR [how_are_you_word] = 'sadness' OR [how_are_you_word] = 'joy' OR [how_are_you_word] = 'disgust' OR [how_are_you_word] = 'surprise' OR [how_are_you_word] = 'trust' OR [how_are_you_word] = 'anticipation') 
 )
