f8ad7536fcb7f0d07215ad66992d4bb3edabd106
Sergio <techdoer@gmail.com>
2018-01-12 20:15:33 +0100

    Computes hours slept.

---
+++ mssql/views/View sleep duration.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[View sleep duration] AS
+	
+	SELECT s.end_date_id, s.person_id,
+		   DATEDIFF(hour, convert(DATETIME, convert(DATETIME, LEFT(convert(varchar, convert(nvarchar(8), s.start_date_id), 112), 120)+' '+LEFT(convert(varchar, s.start_timestamp, 108), 8), 20), 120), 
+		   convert(DATETIME, convert(DATETIME, LEFT(convert(varchar, convert(nvarchar(8), s.end_date_id), 112), 120)+' '+LEFT(convert(varchar, s.end_timestamp, 108), 8), 20), 120)) AS Duration
+	
+	FROM [dbo].[Sleep log] s;
+
+  
+
+  
