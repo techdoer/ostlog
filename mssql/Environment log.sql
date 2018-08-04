f5bf10e6704126fcf778aa5ada42c5769c578ef1
Sergio <techdoer@gmail.com>
2018-08-04 13:31:38 +0200

    Used to log environmental measures

---
+++ mssql/Environment log.sql
@@ -0,0 +1,39 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: Each entry records environmental measures at a particular location.  Supports Azure SQL Server.
+-- URL: http://ostlog.org/schema/Environment_log.json
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2017 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Environment log]
+(
+	[id] INT NOT NULL,
+	[meta_id] INT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[timestamp] TIME(0) NOT NULL,
+	[domain] NVARCHAR (15) NOT NULL,
+	[location_id] INT NOT NULL, 
+	[temperature_celsius] FLOAT NULL,
+	[humidity_relative_%] FLOAT NULL,
+	[sound_decibals] FLOAT NULL,
+	[remarks] NVARCHAR (4000) NULL,
+	CONSTRAINT [PK_Environment log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Environment log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Environment log-People] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [FK_Environment log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
+
+	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
+)
