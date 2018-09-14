9debc5d144ef2f1969155241cf2d0046f75f0f70
Sergio <techdoer@gmail.com>
2018-09-14 22:32:44 +0200

    facilitates data processing

---
+++ mssql/views/View continuous learning duration.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[View continuous learning duration] AS
+	
+	SELECT cl.id, 
+		cd.full_date as startDate, 
+		cd1.full_date as endDate,
+		cl.person_id, 
+		DATEPART(year, cd.full_date) AS startYear, 
+		DATEPART(week, cd.full_date) AS startWeekNum, 
+		DATEPART(year, cd1.full_date) AS endYear,
+		DATEPART(week, cd1.full_date) AS endWeekNum,
+		DATEDIFF(week, cd.full_date, cd1.full_date) AS durationWeeks
+	
+FROM [dbo].[Continuous learning log] cl INNER JOIN [dbo].[Calendar dates] cd on cl.start_date_id = cd.date_id
+     INNER JOIN [dbo].[Calendar dates] cd1 on cl.end_date_id = cd1.date_id
