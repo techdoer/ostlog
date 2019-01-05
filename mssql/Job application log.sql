727bd4faeaa1f22165a181fab713c4c32eca91de
Sergio <techdoer@gmail.com>
2019-01-05 14:26:53 +0100

    Tracking job applications.

---
+++ mssql/Job application log.sql
@@ -0,0 +1,40 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: Each entry records a submitted job application.  Supports Azure SQL Server.
+-- URL: http://ostlog.org/schema/Chores_log.json
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2019 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Job application log]
+(
+	[id] INT NOT NULL,
+	[meta_id] INT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[timestamp] TIME(0) NOT NULL,
+	[location_id] INT NOT NULL,
+	[employer_location_id] INT NOT NULL,
+	[domain] NVARCHAR (15) NOT NULL,
+	[person_id] INT NOT NULL, 
+	[job_description] NVARCHAR (4000) NOT NULL,
+	[remarks] NVARCHAR (4000) NULL,
+	CONSTRAINT [PK_Job application log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Job application log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Job application log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+	CONSTRAINT [FK_Job application log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [FK_Job application log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
+
+	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental'),
+)
