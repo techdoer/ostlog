6c942845c1f42d9a1b500e1718237167fc2259f4
Sergio <techdoer@gmail.com>
2019-01-11 06:31:16 +0100

    Noise recordings in decibals

--- mssql/Noise log.sql
+++ mssql/Noise log.sql
@@ -12,13 +12,13 @@
 -- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
 -- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
--- Version: Each entry records weather-related measures at a particular location.  Supports Azure SQL Server.
+-- Version: Each entry records noise levels  at a particular location.  Supports Azure SQL Server.
 -- URL: http://ostlog.org/schema/Weather_log.json
 --
 -- Authors:     Sergio Bogazzi
--- Copyright:   Copyright (c) 2017 family•smarts
+-- Copyright:   Copyright (c) 2019 family•smarts
 -- License:     MIT License
-CREATE TABLE [dbo].[Weather log]
+CREATE TABLE [dbo].[Noise log]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
 	[meta_id] INT NULL,
@@ -26,17 +26,12 @@ CREATE TABLE [dbo].[Weather log]
 	[timestamp] TIME(0) NOT NULL,
 	[domain] NVARCHAR (15) NOT NULL,
 	[location_id] INT NOT NULL, 
-	[condition] NVARCHAR (512) NULL,
-	[temperature_celsius] FLOAT NULL,
-	[humidity_relative_%] FLOAT NULL,
-	[pressure_mb] FLOAT NULL,
-	[sound_decibals] FLOAT NULL,
+	[noise_decibals] FLOAT NULL,
 	[remarks] NVARCHAR (4000) NULL,
-	CONSTRAINT [PK_Weather log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Weather log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Weather log-People] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
-	CONSTRAINT [FK_Weather log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
-	CONSTRAINT [FK_Weather log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [PK_Noise log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Noise log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Noise log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
+	CONSTRAINT [FK_Noise log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
 
 	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
 )
