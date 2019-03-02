3282604c5372c3312f87f2a776354751dd0eaace
Sergio <techdoer@gmail.com>
2019-03-02 13:59:07 +0100

    Tracking air quality and common variables

---
+++ mssql/Air quality log.sql
@@ -0,0 +1,43 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2017 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: Each entry records air-quality related measures at a particular location.  Supports Azure SQL Server.
+-- URL: http://ostlog.org/schema/Air_quality_log.json
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2019 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Air quality log]
+(
+	[id] INT IDENTITY (1,1) NOT NULL,
+	[meta_id] INT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[timestamp] TIME(0) NOT NULL,
+	[domain] NVARCHAR (15) NOT NULL,
+	[location_id] INT NOT NULL, 
+	[o3_ug_m3] FLOAT NULL,
+	[no2_ug_m3] FLOAT NULL,
+	[so2_ug_m3] FLOAT NULL,
+	[co_ug_m3] FLOAT NULL,
+	[pm10_ug_m3] FLOAT NULL,
+	[pm25_ug_m3] FLOAT NULL,
+	[remarks] NVARCHAR (4000) NULL,
+	CONSTRAINT [PK_Air quality log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Air quality log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Air quality log-People] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [FK_Air quality8 log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
+	CONSTRAINT [FK_Air quality log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+
+	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
+)
