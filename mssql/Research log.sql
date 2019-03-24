518e04f6edb59e8d3944cc1b5937d324c3d494cc
Sergio <techdoer@gmail.com>
2019-03-24 06:19:05 +0100

    Cleaning up and adding a Research logger to capture nuggets of well-being research.

---
+++ mssql/Research log.sql
@@ -0,0 +1,40 @@
+﻿--
+-- This program is licensed to you under the MIT License
+-- Copyright (c) 2019 family•smarts. All rights reserved.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+-- of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- Version: Each entry records the status of a planned or completed chore.  Supports Azure SQL Server.
+-- URL: http://ostlog.org/schema/Research_log.json
+--
+-- Authors:     Sergio Bogazzi
+-- Copyright:   Copyright (c) 2019 family•smarts
+-- License:     MIT License
+CREATE TABLE [dbo].[Research log]
+(
+	[id] INT NOT NULL,
+	[meta_id] INT NULL,
+	[calendar_date_id] INT NOT NULL,
+	[timestamp] TIME(0) NOT NULL,
+	[location_id] INT NULL,
+	[domain] NVARCHAR (15) NOT NULL,
+	[summary_text] NVARCHAR (4000) NOT NULL,
+	[full_text] TEXT NULL,
+	[url] NVARCHAR(1000) NULL,
+	[keywords_csv] NVARCHAR (1000) NOT NULL,
+	[remarks] NVARCHAR (4000) NULL,
+	CONSTRAINT [PK_Research log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Research log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Research log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [FK_Research log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
+
+	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental'),
+)
