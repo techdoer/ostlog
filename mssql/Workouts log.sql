87b554a32054be5ae840627284e3225289d03746
Sergio <techdoer@gmail.com>
2019-12-31 16:27:30 +0100

    More workout options

--- mssql/Workouts log.sql
+++ mssql/Workouts log.sql
@@ -27,6 +27,6 @@ CREATE TABLE [dbo].[Workouts log]
 	CONSTRAINT [FK_Workouts log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
 	CONSTRAINT [FK_Workouts log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
 
-	CHECK ([workout_type]='walking' OR [workout_type]='running' OR [workout_type]='swimming' OR [workout_type]='cycling' OR [workout_type]='hiking' OR [workout_type]='gymnastics' OR [workout_type]='dance' OR [workout_type]='roller-blading' OR [workout_type]='other'),
+	CHECK ([workout_type]='walking' OR [workout_type]='running' OR [workout_type]='swimming' OR [workout_type]='cycling' OR [workout_type]='hiking' OR [workout_type]='gymnastics' OR [workout_type]='dance' OR [workout_type]='roller-blading' OR [workout_type]='roller-skating' OR [workout_type]='other'),
 	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
 )
