2282e1cc63555b0a0c5ff896474ab62c634242d1
Sergio <sergio@familysmarts.onmicrosoft.com>
2020-02-07 10:08:28 +0000

    enables grouping multiple locations as one

--- mssql/Location.sql
+++ mssql/Location.sql
@@ -1,4 +1,4 @@
-﻿--
+--
 -- Version: A specific place on earth.  Designed for Azure SQL Server. 
 -- URL: http://ostlog.org/schema/Calendar_dates.json
 --
@@ -9,6 +9,7 @@ CREATE TABLE [dbo].[Location]
 	[country_iso3_code] NVARCHAR (3) NOT NULL,
 	[latitude] FLOAT NULL,
 	[longitude] FLOAT NULL,
+	[custom_group_1] NVARCHAR (200) NULL,
 	[remarks] NVARCHAR(2056) NULL,
 	CONSTRAINT [PK Location] PRIMARY KEY ([location_id]),
 )
