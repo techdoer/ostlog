427f283a11d2b05068fa9488a1e41c596372250a
Sergio <techdoer@gmail.com>
2020-03-13 14:27:44 +0100

    Enabling time periods

--- mssql/Story log.sql
+++ mssql/Story log.sql
@@ -1,20 +1,23 @@
-﻿--
--- Version: Each entry is a narrative regarding an observation or outcome in time.  Supports Azure SQL Server.
--- URL: http://ostlog.org/schema/Story_log.json
+--
+-- URL: https://github.com/techdoer/ostlog/blob/master/schema/Story_log.json
 --
 CREATE TABLE [dbo].[Story log]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
 	[meta_id] INT NULL,
-	[calendar_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
-	[timestamp] TIME(0) NOT NULL,
+	[start_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
+	[start_time] TIME(0) NOT NULL,
+	[end_date_id] INT NULL, 
+	[end_time] TIME(0) NULL,
 	[location_id] INT NULL,
 	[domain] NVARCHAR (15) NOT NULL,
 	[person_id] INT NOT NULL, 
-	[text] NVARCHAR(4000) NULL, 
+	[title] NVARCHAR (512) NULL,
+	[text_summary] NVARCHAR(4000) NULL, 
 	[remarks] NVARCHAR (4000) NULL,
 	CONSTRAINT [PK_Story log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Story log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Story log-Calendar_date] FOREIGN KEY ([start_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Story log-Calendar_date1] FOREIGN KEY ([end_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_Story log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
 	CONSTRAINT [FK_Story log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
 	CONSTRAINT [FK_Story log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
