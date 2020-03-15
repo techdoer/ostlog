651aa3e601d85c4568eed6a849bf9b5173ba498d
Sergio <techdoer@gmail.com>
2020-03-15 16:04:39 +0100

    Standardizing on FHIR Timing type

--- mssql/Therapy log.sql
+++ mssql/Therapy log.sql
@@ -16,7 +16,7 @@ CREATE TABLE [dbo].[Therapy log]
 	[therapy_sctid] INT NOT NULL, -- SNOWMED CT ID referencing a procedure
 	
 	-- begin columns based on FHIR Timing type - an event that may occur multiple times 
-	[boundary_range_start_date] INT CONVERT (char(8), GETDATE(), 112) NULL,
+	[boundary_range_start_date] INT NULL,
 	[boundary_range_start_time] TIME(0) NULL,
 	[boundary_range_end_date] INT NULL,
 	[boundary_range_end_time] TIME(0) NULL,
@@ -50,8 +50,8 @@ CREATE TABLE [dbo].[Therapy log]
 	-- start FHIR type constraints 
 	CONSTRAINT [FK_Therapy log-Calendar_date2] FOREIGN KEY ([boundary_range_start_date]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_Therapy log-Calendar_date3] FOREIGN KEY ([boundary_range_end_date]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CHECK ([duration_units] = 's', [duration_units] = 'min', [duration_units] = 'h', [duration_units] = 'd', [duration_units] = 'wk', [duration_units] = 'mo', [duration_units] = 'a')  
-	CHECK ([period_units] = 's', [period_units] = 'min', [period_units] = 'h', [period_units] = 'd', [period_units] = 'wk', [period_units] = 'mo', [period_units] = 'a')  
+	CHECK ([duration_units] = 's' OR [duration_units] = 'min' OR [duration_units] = 'h' OR [duration_units] = 'd' OR [duration_units] = 'wk' OR [duration_units] = 'mo' OR [duration_units] = 'a'),
+	CHECK ([period_units] = 's' OR [period_units] = 'min' OR [period_units] = 'h' OR [period_units] = 'd' OR [period_units] = 'wk' OR [period_units] = 'mo' OR [period_units] = 'a'),  
 	-- end FHIR type constraints 
 
 	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental'),
