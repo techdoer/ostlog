81152f4690a7d5867e943388836b7a7f559a69a1
Sergio Bogazzi <techdoer@gmail.com>
2020-03-26 11:56:16 +0100

    Aligning to new Timing schedule

--- mssql/Continuous learning log.sql
+++ mssql/Continuous learning log.sql
@@ -6,10 +6,7 @@ CREATE TABLE [dbo].[Continuous learning log]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
 	[meta_id] INT NULL,
-	[start_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
-	[start_time] TIME (0) NOT NULL,
-	[end_date_id] INT NULL,
-	[end_time] TIME (0) NULL,
+	[schedule_id] INT NOT NULL,
 	[location_id] INT NULL,
 	[domain] NVARCHAR (15) DEFAULT 'physical' NOT NULL,
 	[person_id] INT NOT NULL,
@@ -20,8 +17,7 @@ CREATE TABLE [dbo].[Continuous learning log]
 	[remarks] NVARCHAR(4000) NULL,   
 
 	CONSTRAINT [PK_Continuous learning log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Continuous learning log-Calendar_date2] FOREIGN KEY ([start_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Continuous learning log-Calendar_date3] FOREIGN KEY ([end_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Continuous learning log-Timing schedule] FOREIGN KEY ([schedule_id]) REFERENCES [dbo].[Timing schedule] ([id]),
 	CONSTRAINT [FK_Continuous learning log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
 	CONSTRAINT [FK_Continuous learning log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
 	CONSTRAINT [FK_Continuous learning log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
