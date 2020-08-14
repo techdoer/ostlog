e532d7bd680853fd26758cb9f9da4b08a7ed9e03
Sergio Bogazzi <techdoer@gmail.com>
2020-08-14 08:08:02 +0200

    Simple naming convention

--- mssql/Health log.sql
+++ mssql/Health log.sql
@@ -1,8 +1,8 @@
 ﻿--
 -- Version: Each entry records a health event observed on a day in time.   Supports Azure SQL Server. 
--- URL: http://ostlog.org/schema/Health_observations_log.json
+-- URL: http://ostlog.org/schema/Health_log.json
 --
-CREATE TABLE [dbo].[Health observations log]
+CREATE TABLE [dbo].[Health log]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
 	[meta_id] INT NULL,
@@ -17,13 +17,13 @@ CREATE TABLE [dbo].[Health observations log]
 	[event_icd10_code] NVARCHAR(10), /* the WHO's ICD-10 code for this event */
 	[occurance] INT NULL, /* 1 = first, 2= recurring; 3 = last */
 	[remarks] NVARCHAR(4000) NULL, 
-	CONSTRAINT [PK_Health observations log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Health observations log-Calendar_date] FOREIGN KEY ([observation_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Health observations log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Health observations log-Symptom] FOREIGN KEY ([event_id]) REFERENCES [dbo].[Symptoms] ([id]),
-	CONSTRAINT [FK_Health observations log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
-	CONSTRAINT [FK_Health observations log-Diagnosis log] FOREIGN KEY ([diagnosis_id]) REFERENCES [dbo].[Diagnosis log] ([id]),
-	CONSTRAINT [FK_Health observations log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [PK_Health log] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_Health log-Calendar_date] FOREIGN KEY ([observation_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Health log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+	CONSTRAINT [FK_Health log-Symptom] FOREIGN KEY ([event_id]) REFERENCES [dbo].[Symptoms] ([id]),
+	CONSTRAINT [FK_Health log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
+	CONSTRAINT [FK_Health log-Diagnosis log] FOREIGN KEY ([diagnosis_id]) REFERENCES [dbo].[Diagnosis log] ([id]),
+	CONSTRAINT [FK_Health log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
 
 	CHECK ([domain] = 'spiritual' OR [domain] = 'social' OR [domain] = 'physical' OR [domain] = 'intellectual' OR [domain] = 'financial' OR [domain] = 'emotional' OR [domain] = 'environmental')
 )
