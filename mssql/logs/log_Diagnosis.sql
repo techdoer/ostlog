9fd1e3066f1c3b3003e1fef476e07fbd7113c5b6
Sergio Bogazzi <techdoer@gmail.com>
2021-12-03 09:13:21 +0100

    Maintenance and updates

--- mssql/logs/log_Diagnosis.sql
+++ mssql/logs/log_Diagnosis.sql
@@ -2,10 +2,9 @@
 -- Version: Each entry charaterizes a diagnosis given by a medical professional. Supports for Azure SQL Server. 
 -- URL: http://ostlog.org/schema/Diagnosis_log.json
 --
-CREATE TABLE [dbo].[Diagnosis log]
+CREATE TABLE [dbo].[log_Diagnosis]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
-	[meta_id] INT NULL,
 	[calendar_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
 	[location_id] INT NULL,
 	[patient_id] INT NOT NULL,
@@ -13,9 +12,8 @@ CREATE TABLE [dbo].[Diagnosis log]
 	[diagnosis_icd10_code] NVARCHAR(10) NOT NULL, /* from the WHO's International Statistical Classification of Diseases and Related Health Problems. */
 	[num_days_with_symptoms] INT NULL,
 	[remarks]	NVARCHAR (2056) NULL,
-	CONSTRAINT [PK_Diagnosis log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Diagnosis log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Diagnosis log-People] FOREIGN KEY ([patient_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Diagnosis log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
-	CONSTRAINT [FK_Diagnosis log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id])
+	CONSTRAINT [PK_log_Diagnosis] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_log_Diagnosis-Calendar_dates] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_log_Diagnosis-People] FOREIGN KEY ([patient_id]) REFERENCES [dbo].[People] ([id]),
+	CONSTRAINT [FK_log_Diagnosis-Locations] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Locations] ([location_id])
 )
