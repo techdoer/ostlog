cc4c5debcb5c44a4a11534af70fdb469d1f5d7cd
Sergio Bogazzi <techdoer@gmail.com>
2021-12-03 13:04:33 +0100

    Updates and maintenance

--- mssql/logs/log_Net_worth.sql
+++ mssql/logs/log_Net_worth.sql
@@ -2,10 +2,9 @@
 -- Version:  Each entry records net worth on  day in time.  Supports Azure SQL Server. 
 -- URL: http://ostlog.org/schema/Net_worth_log.json
 --
-CREATE TABLE [dbo].[Net worth log]
+CREATE TABLE [dbo].[log_Net_worth]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
-	[meta_id] INT NULL,
 	[calendar_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
 	[location_id] INT NULL,
 	[person_id] INT NOT NULL, 
@@ -19,8 +18,7 @@ CREATE TABLE [dbo].[Net worth log]
 	[other_liabilities_usd] INT NULL,
 	[remarks] NVARCHAR(4000) NULL, 
 	CONSTRAINT [PK_Net worth log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Net worth log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Net worth log-Calendar_dates] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
 	CONSTRAINT [FK_Net worth log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Net worth log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
-	CONSTRAINT [FK_Net worth log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id])
+	CONSTRAINT [FK_Net worth log-Locations] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Locations] ([location_id])
 )
