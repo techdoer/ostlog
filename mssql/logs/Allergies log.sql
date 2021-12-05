ac08be8897dfe07053c6510b1a80d854d79a9963
Sergio Bogazzi <techdoer@gmail.com>
2021-12-05 08:07:49 +0100

    Routine update and maintenance

--- mssql/logs/Allergies log.sql
+++ mssql/logs/Allergies log.sql
@@ -1,19 +1,17 @@
 ﻿--
 -- Version: Each entry characterizes a chronic/acute allergic reaction.  Supports Azure SQL Server.
--- URL: http://ostlog.org/schema/Allergy_log.json
+-- URL: http://ostlog.org/schema/log_Allergy.json
 --
-CREATE TABLE [dbo].[Allergies log]
+CREATE TABLE [dbo].[log_Allergies]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
-	[meta_id] INT NULL,
 	[calendar_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
 	[location_id] INT NOT NULL, 
 	[person_id] INT NOT NULL,
 	[allergen_type_sctid] INT NOT NULL, -- SNOWMED CT ID referencing a type of allergen disorder
 	[remarks] NVARCHAR(2056) NULL,   
 	CONSTRAINT [PK_Allergy log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Allergy log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Allergy log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id]),
+	CONSTRAINT [FK_Allergy log-Calendar_dates] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_Allergy log-Locations] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Locations] ([location_id]),
 	CONSTRAINT [FK_Allergy log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Allergy log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id])
 )
