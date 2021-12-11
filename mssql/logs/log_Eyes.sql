4091b6c75d72a1fd84c4466e371a69087e88abd6
Sergio Bogazzi <techdoer@gmail.com>
2021-12-11 09:05:36 +0100

    Routine maintenance

--- mssql/logs/log_Eyes.sql
+++ mssql/logs/log_Eyes.sql
@@ -1,11 +1,10 @@
 ﻿--
 -- Version: Each entry records results of a standard eye exam.  Supports Azure SQL Server. 
--- URL: http://ostlog.org/schema/Eyes_log.json
+-- URL: http://ostlog.org/schema/log_Eyes.json
 --
-CREATE TABLE [dbo].[Eyes log]
+CREATE TABLE [dbo].[log_Eyes]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
-	[meta_id] INT NULL,
 	[calendar_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
 	[timestamp] TIME(0) NOT NULL,
 	[location_id] INT NULL,
@@ -23,9 +22,8 @@ CREATE TABLE [dbo].[Eyes log]
 	[pupillary_distance_mm] FLOAT NULL,
 	[exam_results_url] NVARCHAR (512) NULL,
 	[remarks] NVARCHAR(2056) NULL, 
-	CONSTRAINT [PK_Eyes log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Eyes log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Eyes log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Eyes log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
-	CONSTRAINT [FK_Eyes log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id])
+	CONSTRAINT [PK_log_Eyes] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_log_Eyes-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_log_Eyes-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+	CONSTRAINT [FK_log_Eyes-Locations] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Locations] ([location_id])
 )
--- mssql/logs/log_Heart.sql
+++ mssql/logs/log_Heart.sql
@@ -1,11 +1,10 @@
 ﻿--
 -- Version: Each entry records heart/ECG exam results.  Supports Azure SQL Server. 
--- URL: http://ostlog.org/schema/Heart_log.json
+-- URL: http://ostlog.org/schema/log_Heart.json
 --
-CREATE TABLE [dbo].[Heart log]
+CREATE TABLE [dbo].[log_Heart]
 (
 	[id] INT IDENTITY (1,1) NOT NULL,
-	[meta_id] INT NULL,
 	[calendar_date_id] INT DEFAULT CONVERT (char(8), GETDATE(), 112) NOT NULL,
 	[timestamp] TIME(0) NOT NULL,
 	[location_id] INT NULL,
@@ -31,9 +30,8 @@ CREATE TABLE [dbo].[Heart log]
 	[ecg_sv1_mv] FLOAT NULL, /* amplification millivolts */
 	[exam_results_url] NVARCHAR(512) NULL,
 	[remarks] NVARCHAR(2056) NULL, 
-	CONSTRAINT [PK_Heart log] PRIMARY KEY CLUSTERED ([id] ASC),
-	CONSTRAINT [FK_Heart log-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
-	CONSTRAINT [FK_Heart log-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
-	CONSTRAINT [FK_Heart log-Meta log] FOREIGN KEY ([meta_id]) REFERENCES [dbo].[Meta log] ([id]),
-	CONSTRAINT [FK_Heart log-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Location] ([location_id])
+	CONSTRAINT [PK_log_Heart] PRIMARY KEY CLUSTERED ([id] ASC),
+	CONSTRAINT [FK_log_Heart-Calendar_date] FOREIGN KEY ([calendar_date_id]) REFERENCES [dbo].[Calendar dates] ([date_id]),
+	CONSTRAINT [FK_log_Heart-People] FOREIGN KEY ([person_id]) REFERENCES [dbo].[People] ([id]),
+	CONSTRAINT [FK_log_Heart-Location] FOREIGN KEY ([location_id]) REFERENCES [dbo].[Locations] ([location_id])
 )
